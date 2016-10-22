b51ac8e7c8bd986d1542cb17041c510868a607b5
Jason L Perry <jasper@ambethia.com>
2016-10-22 15:12:51 -0400

    Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../styles/screen.sass'
+
+class App extends Component {
+
+  render () {
+    return <div>
+      <h1>Hello, World!</h1>
+    </div>
+  }
+}
+
+export default App
